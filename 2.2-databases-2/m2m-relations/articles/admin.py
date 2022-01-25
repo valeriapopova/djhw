6d45b14476bd52ebeb86b88becdba105374b6799
valeriapopova <iamvaleriapopova@yandex.ru>
2022-01-25 16:51:44 +0400

    8

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,15 +1,35 @@
 from django.contrib import admin
-
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 from .models import Article, Tag, ArticleTag
 
+
+class ArticleTagInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        counter = 0
+        for form in self.forms:
+            for k, v in form.cleaned_data.items():
+                if k == 'is_main' and v is True:
+                    counter += 1
+        if counter > 1:
+            raise ValidationError('Основным может быть только один раздел')
+        elif counter < 1:
+            raise ValidationError('Укажите основной раздел')
+        return super().clean()
+
+
 class ArticleTagInline(admin.TabularInline):
-    model =
+    model = ArticleTag
+    formset = ArticleTagInlineFormset
+
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'title', 'text']
+    inlines = [ArticleTagInline, ]
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'name']
+    list_filter = ['name']
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -12,6 +12,7 @@ class Article(models.Model):
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
@@ -31,9 +32,10 @@ class Tag(models.Model):
 
 class ArticleTag(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes', verbose_name='Раздел')
     is_main = models.BooleanField(default=False, verbose_name='Основной')
 
     class Meta:
         verbose_name = 'Тематика статьи'
         verbose_name_plural = 'Тематики статьи'
+        ordering = ['-is_main', 'tag']

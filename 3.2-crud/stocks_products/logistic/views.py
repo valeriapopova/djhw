27698b202751c233cd0895cb99426e4b314ec78b
valeriapopova <iamvaleriapopova@yandex.ru>
2022-02-04 16:30:08 +0400

    1

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,16 +1,19 @@
+from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
-
-from logistic.models import Product, Stock
-from logistic.serializers import ProductSerializer, StockSerializer
+from .models import Product, Stock
+from .serializers import ProductSerializer, StockSerializer
 
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    # при необходимости добавьте параметры фильтрации
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'description']
 
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
-    # при необходимости добавьте параметры фильтрации
+    filter_backends = [SearchFilter]
+    SearchFilter.search_param = 'products'
+    search_fields = ['products__title', 'products__description']
